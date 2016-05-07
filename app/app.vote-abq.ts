99b469ca408aea93bcfc9db7be10cdf9f0d9c5f2
Dylan McDonald <dmcdonald21@cnm.edu>
2016-05-07 13:37:38 -0600

    Added router configuration

--- app/app.vote-abq.ts
+++ app/app.vote-abq.ts
@@ -2,12 +2,18 @@ import {Component} from "angular2/core";
 import {VotingList} from "./voting-list";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {VotingService} from "./voting.service";
+import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 
 @Component({
 	selector: "vote-abq",
 	templateUrl: "app/templates/splash-page.html",
-	directives: [VotingList],
+	directives: [ROUTER_DIRECTIVES, VotingList],
 	providers: [HTTP_PROVIDERS, VotingService]
 })
+
+@RouteConfig([
+	{path: "/", name: "VoteAbqSplash", component: VoteAbq},
+	{path: "/voting/:id", name: "VoteAbqDetail", component: VotingList}
+])
 
 export class VoteAbq {}
